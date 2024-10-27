959dd55f544e9b2282fd89e5d092fcc11aacf659
Zehra Kaya <nzhehrakaya@gmail.com>
2024-10-27 12:50:01 +0300

    Düzeltmeler yapıldı

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import imageio.v3 as iio  # imageio v3 sürümünü kullanıyorsanız
+import imageio.v3 as iio
 import numpy as np
 from PIL import Image
 
-# Görüntü dosyalarını yükle
-image_files = ["png1.png", "png2.png", "png3.png"]  # Bu kısma görüntü dosya adlarınızı ekleyin
 
-# Görüntüleri bir listeye yükleyin
+image_files = ["png1.png", "png2.png", "png3.png"]
+
+
 images = []
 
 for file_name in image_files:
-    img = Image.open(file_name)   # Görüntüyü aç
-    img_rgb = img.convert("RGB")  # RGB formatına dönüştür
-    img_array = np.array(img_rgb)  # NumPy dizisine dönüştür
-    images.append(img_array)  # Listeye ekle
+    img = Image.open(file_name)
+    img_rgb = img.convert("RGB")
+    img_array = np.array(img_rgb)
+    images.append(img_array)
 
-# GIF oluştur
-iio.imwrite("output.gif", images, duration=0.5,loop=0)  # duration parametresi her kare arasındaki gecikmeyi belirler
+
+iio.imwrite("output.gif", images, duration=0.5,loop=0)
